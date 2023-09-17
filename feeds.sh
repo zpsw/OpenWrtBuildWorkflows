64c4d7c1e74a0d4d44e1c18b0180d954d70a0f49
zpsw <zpswing@gmail.com>
2023-09-17 22:12:23 +0800

    commit

--- feeds.sh
+++ feeds.sh
@@ -38,3 +38,18 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' >> feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+
+
+# 删除自定义源默认的 argon 主题
+rm -rf package/lean/luci-theme-argon
+
+# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
+find ./ -name luci-theme-argon | xargs rm -rf;
+
+# 针对 LEDE 项目拉取 argon 原作者的源码
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+# 替换默认主题为 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
+sed -i 's/192.168.1.1/192.168.6.3/g' package/base-files/files/bin/config_generate
