ed26f0e81f993ecf52d5d24751a23035a5c25592
吕志鹏 <lvzhipeng1@gmail.com>
2023-09-18 08:35:21 +0800

    commit

--- feeds.sh
+++ feeds.sh
@@ -36,5 +36,6 @@
 #EOF
 #git apply example.patch
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' >> feeds.conf.default
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
