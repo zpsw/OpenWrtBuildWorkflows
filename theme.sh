06cedf261db4a180a29294127ee6e6de123e075d
zpsw <zpswing@gmail.com>
2023-10-08 10:05:32 +0800

    change lan ip

--- theme.sh
+++ theme.sh
@@ -28,4 +28,4 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 # 替换默认主题为 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
-sed -i 's/192.168.1.1/192.168.6.3/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.4/g' package/base-files/files/bin/config_generate
